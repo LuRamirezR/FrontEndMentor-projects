6d3b1f02bd894fdf1a1d7da9d0c8f8e64a53b5b4
LuisaRami2018 <luisa.ramirezrpo10@gmail.com>
2024-08-28 15:32:14 -0500

    Styles for mobile view

--- 05-article-preview/share.js
+++ 05-article-preview/share.js
@@ -1,22 +1,40 @@
 let shareContainer = document.getElementById("author-container");
 let shareActive = document.getElementById("share-active");
+
 function shareNavBar() {
-    if(shareContainer.style.display === "none") {
-        shareContainer.style.display = "block";
-        shareActive.style.display = "none";
-    }else {
-        shareContainer.style.display = "none";
-        shareActive.style.display = "block";
+    if (shareContainer.classList.contains("visible")) {
+        shareContainer.classList.remove("visible");
+        shareContainer.classList.add("hidden");
+        shareActive.classList.remove("hidden");
+        shareActive.classList.add("visible");
+    } else {
+        shareContainer.classList.remove("hidden");
+        shareContainer.classList.add("visible");
+        shareActive.classList.remove("visible");
+        shareActive.classList.add("hidden");
     }
 }
 
 function showAuthor() {
-    // let shareActive = document.getElementById("share-active");
-    if(shareActive.style.display === "none") {
-        shareActive.style.display = "block";
-        shareContainer.style.display = "none";
-    }else {
-        shareActive.style.display = "none";
-        shareContainer.style.display = "flex";
-    }
+    shareNavBar();
 }
+
+// function shareNavBar() {
+//     if(shareContainer.style.display === "none") {
+//         shareContainer.style.display = "flex";
+//         shareActive.style.display = "none";
+//     }else {
+//         shareContainer.style.display = "none";
+//         shareActive.style.display = "flex";
+//     }
+// }
+
+// function showAuthor() {
+//     if(shareActive.style.display === "none") {
+//         shareActive.style.display = "block";
+//         shareContainer.style.display = "none";
+//     }else {
+//         shareActive.style.display = "none";
+//         shareContainer.style.display = "flex";
+//     }
+// }
