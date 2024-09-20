f367b9176366ee9ebd5a22c0290430c2072b9312
LuisaRami2018 <luisa.ramirezrpo10@gmail.com>
2024-09-20 17:24:27 -0500

    Adding tooltip config on laptop mediaquery

--- 05-article-preview/share.js
+++ 05-article-preview/share.js
@@ -2,39 +2,17 @@ let shareContainer = document.getElementById("author-container");
 let shareActive = document.getElementById("share-active");
 
 function shareNavBar() {
-    if (shareContainer.classList.contains("visible")) {
-        shareContainer.classList.remove("visible");
-        shareContainer.classList.add("hidden");
-        shareActive.classList.remove("hidden");
-        shareActive.classList.add("visible");
+    if ( window.innerWidth >= 1024 ) {
+        shareActive.classList.toggle("hidden");
+        shareActive.classList.toggle("visible");
     } else {
-        shareContainer.classList.remove("hidden");
-        shareContainer.classList.add("visible");
-        shareActive.classList.remove("visible");
-        shareActive.classList.add("hidden");
+        shareContainer.classList.toggle("visible");
+        shareContainer.classList.toggle("hidden");
+        shareActive.classList.toggle("hidden");
+        shareActive.classList.toggle("visible");
     }
 }
 
 function showAuthor() {
     shareNavBar();
 }
-
-// function shareNavBar() {
-//     if(shareContainer.style.display === "none") {
-//         shareContainer.style.display = "flex";
-//         shareActive.style.display = "none";
-//     }else {
-//         shareContainer.style.display = "none";
-//         shareActive.style.display = "flex";
-//     }
-// }
-
-// function showAuthor() {
-//     if(shareActive.style.display === "none") {
-//         shareActive.style.display = "block";
-//         shareContainer.style.display = "none";
-//     }else {
-//         shareActive.style.display = "none";
-//         shareContainer.style.display = "flex";
-//     }
-// }
